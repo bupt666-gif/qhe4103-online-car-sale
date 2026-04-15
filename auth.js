edbb5ba5c07097721e952f1b2b5e76a11d9078ec
Gefeitao865 <12920337@qq.com>
2026-04-15 22:18:45 +0800

    Delete auth.js

--- auth.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded',function(){
-    const form=document.getElementById('loginForm');
-    form.addEventListener('submit',function(e){
-        e.preventDefault();
-        alert('Login Success!');
-        window.location.href='add-car.html';
-    });
-});
